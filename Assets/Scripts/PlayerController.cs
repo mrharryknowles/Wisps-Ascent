2c0b8b2edc2a0a79ba4b52e6075c7e640bf67e0e
mrharryknowles <harryknowles7@icloud.com>
2025-05-14 20:41:00 +0100

    Horizontal movement and ability to charge jumps (prototype)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float minJump = 5f;
+    public float maxJump = 15f;
+    public float chargeRate = 10f;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    private float jumpPower = 0f;
+    private bool charging = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-     
+        // horizontal movement
+        float move = Input.GetAxisRaw("Horizontal"); // a/d or left/right
+        if (isGrounded)
+        {
+            rb.velocity = new Vector3(move * moveSpeed, rb.velocity.y, 0f);
+        }
+
+        // handles the charging of jumps
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            charging = true;
+            jumpPower = minJump;
+        }
+
+        // the charge increases as the space bar is held down
+        if (Input.GetKey(KeyCode.Space) && charging)
+        {
+            jumpPower += chargeRate * Time.deltaTime;
+            jumpPower = Mathf.Clamp(jumpPower, minJump, maxJump);
+        }
+
+        // release the space bar to finish the jump
+        if (Input.GetKeyUp(KeyCode.Space) && charging)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, 0f); // reset vertical speed
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            charging = false;
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // checks whether the player is grounded
+        if (collision.contacts[0].normal == Vector3.up)
+        {
+            isGrounded = true;
+        }
     }
 }
-
